03cb58daa5c0d7ad73076aea84084b4e763836ce
iti98 <etienne98.ch@gmail.com>
2023-01-03 11:20:29 +0100

    Adding register and login for usser

---
+++ api/controllers/userController.js
@@ -0,0 +1,63 @@
+const jwt = require("jsonwebtoken");
+const User = require('../models/userModel');
+
+userRegister = (req, res) => {
+    let newUser = new User(req.body);
+    console.log(req.body);
+
+    newUser.save((error, user) => {
+        if (error) {
+            res.status(401);
+            console.log(error);
+            res.send({ message: "Reqûete invalide." });
+            
+        }
+        else {
+            res.status(201);
+            res.send({ message: `Utilisateur crée : ${user.email}` });
+        }
+    })
+}
+
+userLogin = (req, res) => {
+    // Find user
+    User.findOne({ email: req.body.email }, (error, user) => {
+        if (error) {
+            // User not found
+            res.status(500);
+            console.log(error);
+            res.json({ message: "Utilisateur non trouvé" });
+        }
+        else {
+            // User found
+            if (user.email == req.body.email && user.password == req.body.password) {
+                // Password correct
+                let userData = {
+                    id: user._id,
+                    email: user.email,
+                    role: "admin"
+                }
+                jwt.sign(userData, process.env.JWT_KEY, { expiresIn: "30 days" }, (error, token) => {
+                    if (error) {
+                        res.status(500)
+                            .json({ message: "Impossible de générer le token" });
+                        console.log(error);
+                    }
+                    else {
+                        res.status(200)
+                            .json({ token });
+                    }
+                })
+            }
+            else {
+                // Password don't match
+                res.status(401)
+                    .json({ message: "Email ou Mot de passe incorrect" });
+                console.log(error);
+
+            }
+        }
+    });
+}
+
+module.exports = { userRegister, userLogin }
--- app.js
+++ app.js
@@ -4,17 +4,23 @@ require('dotenv').config();
 var cors = require('cors');
 
 
+const userRoute = require("./api/routes/userRoute");
+
+
 const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = express();
 
 // mongoose.connect(process.env.MongoDB); // Sans docker
-// mongoose.connect('mongodb://mongo/nodeapi'); // Avec docker
+mongoose.connect(process.env.MongoDB); // Sans docker
 
 server.use(express.urlencoded());
 server.use(express.json());
 
 server.use(cors()); 
+
+userRoute(server);
+
 
 server.listen(port, hostname);
