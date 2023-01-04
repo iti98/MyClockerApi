0ec69ac4105915fefcd853aba177fce2374444a5
iti98 <etienne98.ch@gmail.com>
2023-01-04 14:34:17 +0100

    Fixing bug from projectRoute

--- api/routes/projectRoute.js
+++ api/routes/projectRoute.js
@@ -2,18 +2,18 @@ module.exports = (server) => {
   const projectController = require("../controllers/projectController");
 
   // All project
-  server.get("/projects", projectController.findAll());
+  server.get("/projects", projectController.findAll);
 
   // A single project with id
-  server.get("/project/:id", projectController.findOne());
+  server.get("/project/:id", projectController.findOne);
 
   // Create a new project
-  server.post("/project", projectController.create());
+  server.post("/project", projectController.create);
 
   // Update a project with id
-  server.put("/project/:id", projectController.update());
+  server.put("/project/:id", projectController.update);
 
   // Delete a project with id
-  server.delete("/project/:id", projectController.deleteOne());
+  server.delete("/project/:id", projectController.deleteOne);
 
 };
